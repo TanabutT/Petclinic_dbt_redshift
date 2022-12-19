feca461108823817888701668e3a4346692c3e35
TanabutT <t_tanabut@hotmail.com>
2022-12-19 17:09:37 +0000

    hh

---
+++ dbt_connect_redshift/models/petclinic_staging/stg3_phistorywithkey.sql
@@ -0,0 +1,26 @@
+with stg2_phistory as (
+
+    SELECT * FROM {{ ref('stg2_phistory')}}
+),
+
+
+
+final as (
+
+    SELECT 
+        ROW_NUMBER() OVER 
+            ( 
+                ORDER BY date_treated,petid ASC
+                 
+            ) AS treatmentcase_key
+        ,* ,petid
+        ,.date_treated
+        ,.proceduretype
+        ,.proceduresubcode
+        ,.procedure_key
+    FROM historynokey
+    LEFT JOIN dimprocedure using (proceduretype,proceduresubcode)
+
+)
+
+SELECT * FROM final
