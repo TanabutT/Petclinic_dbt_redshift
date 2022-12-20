d73d83593ccc0450ed9c329747b00dc5a60f34bb
TanabutT <t_tanabut@hotmail.com>
2022-12-20 02:53:45 +0000

    dimdate trial

--- dbt_connect_redshift/models/petclinic_staging/dimdate.sql
+++ dbt_connect_redshift/models/petclinic_staging/dimdate.sql
@@ -11,7 +11,7 @@ final as (
                 ORDER BY date_treated ASC
                  
             ) AS date_key
-        ,convert(datetime, date_treated, 103) as date_treated
+        ,DISTINCT convert(datetime, date_treated, 23) as date_treated
 
     FROM history
 )
@@ -20,5 +20,7 @@ final as (
 -- 111 - the one you are using is Japan yy/mm/dd.
 
 -- I guess the one you are looking for is 103, that is dd/mm/yyyy
+-- 23	ISO8601	yyyy-mm-dd
+-- ref: https://learn.microsoft.com/en-us/sql/t-sql/functions/cast-and-convert-transact-sql?view=sql-server-2017#date-and-time-styles
 
 SELECT * FROM final
